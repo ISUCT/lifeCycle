0bfec7360e2ff78151999651871fb49840613626
AlexCPU7 <AlexCPU7@mail.ru>
2016-10-04 23:35:19 +0300

    All

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,7 +8,44 @@ jQuery('document').ready(function() {
     jQuery('body').append(a);
     //Копирует p*/
     
-    jQuery('Button').on('click', function(){        
+    jQuery('input').on('keyup', function(){        
+        var d, d0, d1, d2;
+        
+        a = jQuery('#a').val();
+        b = jQuery('#b').val();
+        c = jQuery('#c').val();
+        
+        a = parseInt(a);
+        b = parseInt(b);
+        c = parseInt(c);
+        
+        d = b*b - 4*a*c;        
+        d0 = -b/2*a;
+		d1 = (-b+(Math.sqrt(d)))/(2*a);
+		d2 = (-b-(Math.sqrt(d)))/(2*a);
+		
+        jQuery('#div1').html(
+			'D = b^2 - 4ac = ' + b + '^2 - 4 * ' + a + ' * ' + c + ' = ' + d);
+		
+		if (d == 0){
+			jQuery('#div2').html(
+				'D = 0, то х = ' + d0
+			);
+		}else{
+			if (d>0){
+				jQuery('#div2').html(
+					'D > 0, то x1 = ' + d1 + ', x2 = ' + d2
+				);
+			}else{
+				jQuery('#div2').html(
+					'D < 0, то решений нет'
+				);
+			}
+		}
+		
+    });
+	
+	jQuery('Button').on('click', function(){        
         var x1, x2, x3, sum;
         
         x1 = jQuery('#x1').val();
